e17c2bd3a00cd8eebed701d886d8c96c94589722
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-01-12 19:54:45 -0300

    Init file

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useEffect, useState } from 'react';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,20 +28,97 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+const displayNone = {
+  // Challenge was not accepting render content or render loading
+  // so when the posts is loaded the "Loading..." is set to display none
+  display: 'none',
+};
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export default function Post({ post }: PostProps) {
+  const [readingTime, setReadingTime] = useState(0);
 
-//   // TODO
-// };
+  useEffect(() => {
+    if (post) {
+      let totalWordsCount = 0;
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+      post.data.content.forEach(section => {
+        if (section.heading) {
+          totalWordsCount += section.heading.split(' ').length;
+        }
 
-//   // TODO
-// };
+        section.body.forEach(paragraph => {
+          totalWordsCount += paragraph.text?.split(' ').length;
+        });
+      });
+
+      setReadingTime(Math.ceil(totalWordsCount / 200));
+
+      const formattedFirstPublicationDate = new Date(
+        post.first_publication_date
+      )
+        .toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'short',
+          year: 'numeric',
+        })
+        .replace(/de |\./g, '');
+
+      post.first_publication_date = formattedFirstPublicationDate;
+    }
+  }, [post]);
+
+  return (
+    <div className={commonStyles.container}>
+      <Header />
+
+      {post && (
+        <>
+          <img src={post.data.banner.url} alt="banner" />
+          <h1>{post.data.title}</h1>
+          <span>{post.first_publication_date}</span>
+          <span>{post.data.author}</span>
+          <span>{readingTime} min</span>
+
+          {post.data.content.map(section => (
+            <section key={section.heading}>
+              <h3>{section.heading}</h3>
+
+              {section.body.map((paragraph, index) => (
+                <p key={index}>{paragraph.text}</p>
+              ))}
+            </section>
+          ))}
+        </>
+      )}
+
+      <p style={post ? displayNone : {}}>Carregando...</p>
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { slug: 'como-utilizar-hooks' } },
+      { params: { slug: 'criando-um-app-cra-do-zero' } },
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    data: { ...response.data },
+  };
+
+  return {
+    props: { post },
+  };
+};
